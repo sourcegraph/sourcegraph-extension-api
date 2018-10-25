39590b952d832f4f0faea27231af068f34fe4087
Farhan Attamimi <farhan@sourcegraph.com>
2018-10-24 18:36:14 -0700

    fix: remove commented chaining attempts

--- src/client/providers/queryTransformer.ts
+++ src/client/providers/queryTransformer.ts
@@ -12,7 +12,7 @@ export class QueryTransformerRegistry extends FeatureProviderRegistry<{}, Transf
 }
 
 /**
- * Returns an observable that emits all provider's transformed query whenever any of the last-emitted set of providers emits
+ * Returns an observable that emits a query transformed by all providers whenever any of the last-emitted set of providers emits
  * a query.
  *
  * Most callers should use QueryTransformerRegistry's transformQuery method, which uses the registered search
@@ -27,11 +27,6 @@ export function transformQuery(
             if (providers.length === 0) {
                 return [query]
             }
-            // let currentQuery = providers[0](query).pipe(map(q => q))
-            // for (const provider of providers) {
-            //     const str = currentQuery.subscribe()
-            //     currentQuery = provider()
-            // }
             return providers.reduce(
                 (currentQuery, transformQuery) =>
                     currentQuery.pipe(
@@ -39,7 +34,6 @@ export function transformQuery(
                     ),
                 of(query)
             )
-            // of(providers).pipe(reduce((accumulatedValue, currentProvider) => currentProvider(accumulatedValue)), providers[0](query)))
         })
     )
 }
